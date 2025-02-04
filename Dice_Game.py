9ec05e129ae3de5d19c3a474aae5295f31c39823
18HenryFloyd18 <henryfloyd50@gmail.com>
2025-02-04 15:23:35 -0600

    Upoladed

--- Dice_Game.py
+++ Dice_Game.py
@@ -1,13 +1,24 @@
 import random
 import math
 diceList = [1,2,3,4,5,6]
-print("Welcome To Dice Battle!")
-wannaPlay = str(input("If You Would Like To Play Type The Word Enter:"))
+# print("Welcome To Dice Battle!")
+# wannaPlay = str(input("If You Would Like To Play Type The Word Enter:"))
 
-if(wannaPlay == "Enter"):
-    print("Welcome To The Game!")
+# if(wannaPlay == "Enter"):
+#     print("Welcome To The Game!")
+# else:
+#     print("To Play Please Type Enter")
+
+randomComp = random.choice(diceList)
+print("The Number That The Computer Rolled Was",randomComp)
+
+randomPlayer = random.choice(diceList)
+print("The Number That You Rolled Was",randomPlayer)
+
+if(randomComp > randomPlayer):
+    print("The Computer Won This Round!")
 else:
-    print("To Play Please Type Enter")
-random.choice(diceList)
-print(diceList)
+    print("You Won This Round!")
+
+
 
