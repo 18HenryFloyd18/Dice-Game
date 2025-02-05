b1a2a3f67bece7c2631897cd498035aa0451f34a
18HenryFloyd18 <henryfloyd50@gmail.com>
2025-02-05 15:24:02 -0600

    Add files via upload

--- Dice_Game.py
+++ Dice_Game.py
@@ -1,18 +1,9 @@
 import random
 import math
 diceList = [1,2,3,4,5,6]
-compWins = []
-playerWins = []
-win = []
-
-# print("Welcome To Dice Battle!")
-# wannaPlay = str(input("Type 'Enter' If You Would Like To Play:"))
-
-# if(wannaPlay == "Enter" or "enter"):
-#     print("Lets Begin! First To 3 Round Victory's Wins!")
-# else:
-#     print("Type 'Enter' To Begin Playing")
-while win:
+comp_Wins = 0
+player_Wins = 0
+while:
     randomComp = random.choice(diceList)
     print("The Number That The Computer Rolled Was",randomComp)
 
@@ -25,6 +16,19 @@ while win:
        print("You And The Computer Got The Same Numbers, Let's Redo The Round")
     else:
        print("You Won This Round!")
+    
+    if(randomComp > randomPlayer):
+        compWins.append(1)
+        print("Computer Score:",comp_Wins)
+        print("Player's Score:",player_Wins)
+    elif(randomComp == randomPlayer):
+        print("Computer's Score:",comp_Wins)
+        print("Player's Score:",player_Wins)
+    else:
+        playerWins.append(1)
+        print("Computer's Score:",comp_Wins)
+        print("Player's Score:",player_Wins)
+
     
 
 
