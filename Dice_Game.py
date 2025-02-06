93333a244e6f7e23b80bbc339a4299f65ddc1c8a
18HenryFloyd18 <henryfloyd50@gmail.com>
2025-02-06 14:00:48 -0600

    Add files via upload

--- Dice_Game.py
+++ Dice_Game.py
@@ -3,32 +3,33 @@ import math
 diceList = [1,2,3,4,5,6]
 comp_Wins = 0
 player_Wins = 0
-while:
-    randomComp = random.choice(diceList)
-    print("The Number That The Computer Rolled Was",randomComp)
+while True:
+    while(player_Wins < 3 and comp_Wins < 3):
+      randomComp = random.choice(diceList)
+      print("The Number That The Computer Rolled Was",randomComp)
 
-    randomPlayer = random.choice(diceList)
-    print("The Number That You Rolled Was",randomPlayer)
+      randomPlayer = random.choice(diceList)
+      print("The Number That You Rolled Was",randomPlayer)
 
-    if(randomComp > randomPlayer):
-       print("The Computer Won This Round!")
-    elif(randomComp == randomPlayer):
-       print("You And The Computer Got The Same Numbers, Let's Redo The Round")
-    else:
-       print("You Won This Round!")
+      if(randomComp > randomPlayer):
+        print("The Computer Won This Round!")
+      elif(randomComp == randomPlayer):
+        print("You And The Computer Got The Same Numbers, Let's Redo The Round")
+      else:
+        print("You Won This Round!")
     
-    if(randomComp > randomPlayer):
-        compWins.append(1)
+      if(randomComp > randomPlayer):
+        comp_Wins = comp_Wins + 1
         print("Computer Score:",comp_Wins)
         print("Player's Score:",player_Wins)
-    elif(randomComp == randomPlayer):
+      elif(randomComp == randomPlayer):
         print("Computer's Score:",comp_Wins)
         print("Player's Score:",player_Wins)
-    else:
-        playerWins.append(1)
+      else:
+        player_Wins = player_Wins + 1
         print("Computer's Score:",comp_Wins)
         print("Player's Score:",player_Wins)
-
+    
     
 
 
