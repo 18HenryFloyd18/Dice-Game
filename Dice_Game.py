133b54594b25e4bb36171aa2e206c5520327223a
18HenryFloyd18 <henryfloyd50@gmail.com>
2025-02-06 21:45:08 -0600

    Add files via upload

--- Dice_Game.py
+++ Dice_Game.py
@@ -8,7 +8,7 @@ player_Wins = 0
 print("Welcome To Dice Game!") #Welcome The Player
 print("Game Rules: You And The Computer Roll A Dice, Whoever Has The Highest Dice Number Wins The Round. First To Three Wins Takes The Victory!") # Explain The Rules
 
-trynaGame = str(input("If You Would Like To Play Enter 'Game'")) #Ask If They Would Like To Play The Game
+trynaGame = str(input("If You Would Like To Play Enter The Word 'Game':")) #Ask If They Would Like To Play The Game
 if(trynaGame == "Game" or "game"): # If The Input Is Fine, Begin The Game. If It Is Not Then Tell Them How To Play
     print("The Computer Will Roll First")
 else:
@@ -24,31 +24,29 @@ while True: # While Either Of The Players Do Not Have 3 Wins Iterate The Loop
       randomPlayer = random.choice(diceList) # Get A Random Number For The User
       print("The Number That You Rolled Was",randomPlayer) #Print Their Number
 
-      if(randomComp > randomPlayer): #If The 
+      if(randomComp > randomPlayer): #If The Computer's Number Is Higher Than The Players Display That The Computer Won The Round
         print("The Computer Won This Round!")
-      elif(randomComp == randomPlayer):
+      elif(randomComp == randomPlayer):#If The Computer's Number Is Equal To The Player's Redo The Round
         print("You And The Computer Got The Same Numbers, Let's Redo The Round")
-      else:
+      else:# Anything Else Means That The Player Won The Round And Display That
         print("You Won This Round!")
     
-      if(randomComp > randomPlayer):
+      if(randomComp > randomPlayer):# If The Computer Won Add 1 To The Overall Score For The Computer's Variable
         comp_Wins = comp_Wins + 1
-        print("Computer Score:",comp_Wins)
+        print("Computer Score:",comp_Wins)# Print The Computer's Score And Also The Player's Score
         print("Player's Score:",player_Wins)
-      elif(randomComp == randomPlayer):
+      elif(randomComp == randomPlayer):#IIf It Is Equal To Both Numbers Then Don't Do Anything But Display The Score
         print("Computer's Score:",comp_Wins)
         print("Player's Score:",player_Wins)
-      else:
+      else: # If The Player Won The Round Add One To Their Score And Then Display The Current Score
         player_Wins = player_Wins + 1
         print("Computer's Score:",comp_Wins)
         print("Player's Score:",player_Wins)
-    if(player_Wins > comp_Wins):
+    if(player_Wins > comp_Wins):# if The Player Won Then Give Them Kudos OtherWise Tell Them Better Luck Next Time
       print("You Won Congrats!")
     else:
       print("The Computer Won This Game, Better Luck Next Game")
-    break
-    
-    
+    break #Break Out Of The Loop
 
 
 
